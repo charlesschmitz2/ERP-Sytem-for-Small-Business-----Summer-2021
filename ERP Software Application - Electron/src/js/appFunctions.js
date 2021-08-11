54467e0c06a923cd4b5e90ffbfdb6539b017f0ee
charlesschmitz2 <charlieschmitz23@gmail.com>
2021-08-11 10:53:52 -0400

    Trying to get submit button to work and dynamic tables

--- ERP Software Application - Electron/src/js/appFunctions.js
+++ ERP Software Application - Electron/src/js/appFunctions.js
@@ -221,6 +221,7 @@ accountingBtn.addEventListener('click', ()=>{
                         //Loads the table full of the correct object elements taken from the postgres table that have been queried to an array of objects called data, each object represents one entry of the table
                             function loadAccountingTable(data) {
                                 //clears out exisitng table data
+                                //console.log(accoutingTableBody);
                                 while(accoutingTableBody.firstChild){
                                     accoutingTableBody.removeChild(accoutingTableBody.firstChild);
                                 }
@@ -310,6 +311,159 @@ inventoryBtn.addEventListener('click', ()=>{
     ipc.send('inventoryPageActive');
 })
 
+//button that if the accounting page is active so the elements are rendered then it will run the query to load the tables
+var runQuery = document.getElementById('runQueryInventory');
+if(runQuery != null){
+    //this is the event listener that coorelates to the 'Run Query' button on the accounting page
+    runQuery.addEventListener('click', () => {
+        runInventoryPage();
+        console.log("running");
+    })
+}
+
+//takes a query strong and opens connection to postgres where the table functions are then run and displayed to the user
+function runInventoryPage(){
+    /* --------------- Query ---------------*/
+        //Since we have a connection to the database and its table we can query the database to get some value
+        let query1 = `
+            SELECT *
+            FROM product_dimension;
+            
+        `;
+        let query = `
+            SELECT *
+            FROM product_dimension, fact_sales 
+            WHERE product_dimension.productID = fact_sales.productID;
+            
+        `;
+
+    /* --------------- Open and Connect to pool --> load and display correct page contents --> run query and fill in data into correct tables  ---------------*/
+
+    //Database Functionality and Connections, each page click has a query that is executed within this database connection
+    const {Pool, Client} = require('pg');
+
+
+    const pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'ERP_Small_Business_Project',
+        password: 'Cas6768890',
+        port: 5432,
+    });
+
+    pool.on('error', (err, client) => {
+        console.log('Error:', err);
+    });
+
+    doAllTableLoading(query);
+    pool.end();
+
+        function doAllTableLoading(query){
+
+            /* --------------- Create data array to hold query results ---------------*/
+                const data = new Array();
+
+
+            /* --------------- Connect to the pool and run the query within a data array ---------------*/    
+
+                            pool.connect()
+                                .then((client) => {
+                                    client.query(query)
+                                        .then(res => {
+                                            for (let row of res.rows) {
+                                                //console.log(row);
+                                                data.push(row);
+                                            }
+                                            //console.log(data);
+                                            checkData(data);
+                                        })
+                                        .catch(err => {
+                                            console.error(err);
+                                        });
+                                })
+                                .catch(err => {
+                                    console.error(err);
+                                });
+
+                //pool.end(); we are leaving it open so that each time someone chooses to reload or rerun the same query for this pool displayed it will continue to work
+
+
+            /* --------------- Check if the data has been properly pushed, if it has then we can begin to load the table for this page ---------------*/
+
+                            function checkData(dataArray) {
+                                if (dataArray.length > 0){   
+                                    //console.log("Data is loaded!");
+                                    //console.log(dataArray.length)
+                                    loadInventoryTable(dataArray);
+                                    return true;
+                                }
+                                else {
+                                    console.warn("Could Not Load Data :( Go Back and Check your Query");
+                                    return false;
+                                }
+                            }//CheckData
+
+            /* --------------- Load the Corresponding Table Rows with the objects that have been queried ---------------*/
+
+                    //Loads the table full of the correct object elements taken from the postgres table that have been queried to an array of objects called data, each object represents one entry of the table
+                        function loadInventoryTable(data) {
+                            //clears out exisitng table data
+                            //console.log(inventoryTableBody);
+                            while(inventoryTableBody.firstChild){
+                                inventoryTableBody.removeChild(inventoryTableBody.firstChild);
+                            }
+
+                            const headers = Object.keys(data[0])
+                            const thead = document.createElement('thead')
+                            const tr = document.createElement('tr')
+                            let createCell = text => {
+                                let th = document.createElement('th')
+                                text = document.createTextNode(text)
+                                th.appendChild(text)
+                                return th
+                              }
+                              
+                              headers.forEach(header => {
+                                let cell = createCell(header)
+                                //console.log(cell);
+                                tr.appendChild(cell)
+                              })
+                            
+                              thead.appendChild(tr)
+                            
+                              inventoryTableBody.appendChild(thead)
+                            
+                            //Populate the table
+
+                            data.forEach((row) => {
+                                const keys = Object.keys(row);
+                                //console.log(keys);
+
+                                const tr = document.createElement("tr")
+                                keys.forEach((key)=> {
+                                    //console.log(`${key}: ${row[key]}`);
+                                    //console.log(row[key]);
+                                    const td = document.createElement("td");
+                                    td.textContent = row[key];
+                                    tr.appendChild(td);
+                                });
+
+                                inventoryTableBody.appendChild(tr);
+                            });
+
+                        }//loadinventoryTableBody
+            
+
+            /* --------------- Display the Data ---------------*/
+
+            const inventoryTableBody = document.querySelector("#inventoryTable > tbody");
+
+        }//doAllTableLoading 
+
+
+}//runInventoryPage
+
+
 
 //-------------------------------------ORDER ENTRY PAGE-------------------------------------
 orderFormBtn.addEventListener('click', ()=>{
@@ -364,23 +518,50 @@ orderFormBtn.addEventListener('click', ()=>{
 
 */
 
+
+})
+
 var submitBtn = document.getElementById("submitButton");
 console.log(submitBtn);
 if(submitBtn != null) {
-    submitBtn.addEventListener("submit", sendForm(event));
+    submitBtn.addEventListener("click", () => {
+        const form = document.getElementById('myForm');
+        const log = document.getElementById('log');
+        document.form.submit();
+        form.addEventListener('submit', logSubmit);
+
+        //event.preventDefault() // stop the form from submitting
+        //console.log(event);
+    
+        //let firstname = document.getElementById("firstname").value;
+        //ipcRenderer.send('form-submission', firstname)
+
+        function logSubmit(event) {
+            log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+            event.preventDefault();
+          }
+
+    });
 }
 function sendForm(event) {
     event.preventDefault() // stop the form from submitting
+    console.log("running event");
 
     let firstname = document.getElementById("firstname").value;
     ipcRenderer.send('form-submission', firstname)
 }
 
 
-})
-
-
-
+/*
+function logSubmit(event) {
+    log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+    event.preventDefault();
+  }
+  
+  const form = document.getElementById('myForm');
+  const log = document.getElementById('log');
+  form.addEventListener('submit', logSubmit);
+  */
 
 //alert( formData.get('field1') + '-' + formData.get('field2') + '-' + formData.get('field3') + '-' + formData.get('field4') + '-' + formData.get('field5') + '-' + formData.get('field6') + '-' + formData.get('field7') + '-' +formData.get('field8') + '-' + formData.get('field9'));
 
